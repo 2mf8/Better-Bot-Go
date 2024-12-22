36ae35854f240e585320d056aec331750a47d194
一仝 <chun5060@outlook.com>
2024-12-22 20:25:27 +0800

    update

--- onebot/frame.go
+++ onebot/frame.go
@@ -1,10 +1,13 @@
 package onebot
 
+import "github.com/2mf8/Better-Bot-Go/dto"
+
 type Frame struct {
-	BotId  string `json:"bot_id,omitempty"`
-	Echo   string `json:"echo,omitempty"`
-	Ok     bool   `json:"ok,omitempty"`
-	Time   int64  `json:"time,omitempty"`
-	SelfId string `json:"self_id,omitempty"`
-	Data   []byte `json:"data,omitempty"`
+	BotId   string         `json:"bot_id,omitempty"`
+	Echo    string         `json:"echo,omitempty"`
+	Ok      bool           `json:"ok,omitempty"`
+	Time    int64          `json:"time,omitempty"`
+	SelfId  string         `json:"self_id,omitempty"`
+	Data    []byte         `json:"data,omitempty"`
+	Payload *dto.WSPayload `json:"payload,omitempty"`
 }
