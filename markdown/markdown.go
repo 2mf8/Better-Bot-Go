aa6b70f2b9358b933862f80309cf13bebc50df9e
一仝 <chun5060@outlook.com>
2026-04-20 23:04:19 +0800

    修复markdown生成器bug

--- markdown/markdown.go
+++ markdown/markdown.go
@@ -108,7 +108,7 @@ func (r *Template) NewLine() *Template {
 }
 
 func (r *Template) Code(content string) *Template {
-	c := strings.ReplaceAll(strings.ReplaceAll(content, "\t", "\\t"), "\n", "\n")
+	c := strings.ReplaceAll(strings.ReplaceAll(content, "\t", "\t"), "\n", "\n")
 	str := fmt.Sprintf("\n```\n{{.%s}}\n```\n", c)
 	r.Str += str
 	return r
@@ -151,27 +151,27 @@ func Builder() *MarkDown {
 }
 
 /* func (r *MarkDown) MqqApi(content string) *MarkDown {
-	str := fmt.Sprintf("\\n[%s](mqqapi://aio/inlinecmd?command=%s&reply=false&enter=false)", content, url.PathEscape(content))
+	str := fmt.Sprintf("\n[%s](mqqapi://aio/inlinecmd?command=%s&reply=false&enter=false)", content, url.PathEscape(content))
 	r.Str += str
 	return r
 }
 
 func (r *MarkDown) MqqApiAuto(content string) *MarkDown {
-	str := fmt.Sprintf("\\n[%s](mqqapi://aio/inlinecmd?command=%s&reply=false&enter=true)", content, url.PathEscape(content))
+	str := fmt.Sprintf("\n[%s](mqqapi://aio/inlinecmd?command=%s&reply=false&enter=true)", content, url.PathEscape(content))
 	r.Str += str
 	return r
 }
 
 // 需配合 SendMarkdownAtMsg 和 SendMarkdownAndKeyboardAtMsg 使用，否则没有效果
 func (r *MarkDown) MqqApiAt(nickname string, tinyId uint64) *MarkDown {
-	str := fmt.Sprintf("\\n[@%s](mqqapi://markdown/mention?at_type=1&at_tinyid=%v)", nickname, tinyId)
+	str := fmt.Sprintf("\n[@%s](mqqapi://markdown/mention?at_type=1&at_tinyid=%v)", nickname, tinyId)
 	r.Str += str
 	return r
 }
 
 // 需配合 SendMarkdownAtMsg 和 SendMarkdownAndKeyboardAtMsg 使用，否则没有效果
 func (r *MarkDown) MqqApiAtToProfile(nickname string, tinyId uint64) *MarkDown {
-	str := fmt.Sprintf("\\n[@%s](mqqapi://card/show_pslcard?src_type=internal&version=1&uin=%v&crad_type=friend&source=qrcode)", nickname, tinyId)
+	str := fmt.Sprintf("\n[@%s](mqqapi://card/show_pslcard?src_type=internal&version=1&uin=%v&crad_type=friend&source=qrcode)", nickname, tinyId)
 	r.Str = str
 	return r
 } */
@@ -192,25 +192,25 @@ func (r *MarkDown) CmdInput(text, show, isTrue string) *MarkDown {
 }
 
 func (r *MarkDown) Url(name, webUrl string) *MarkDown {
-	str := fmt.Sprintf("\\n[🔗%s](%s)", name, webUrl)
+	str := fmt.Sprintf("\n[🔗%s](%s)", name, webUrl)
 	r.Str += str
 	return r
 }
 
 func (r *MarkDown) H1(content string) *MarkDown {
-	str := fmt.Sprintf("\\n# %s", content)
+	str := fmt.Sprintf("\n# %s", content)
 	r.Str += str
 	return r
 }
 
 func (r *MarkDown) H2(content string) *MarkDown {
-	str := fmt.Sprintf("\\n## %s", content)
+	str := fmt.Sprintf("\n## %s", content)
 	r.Str += str
 	return r
 }
 
 func (r *MarkDown) H3(content string) *MarkDown {
-	str := fmt.Sprintf("\\n### %s", content)
+	str := fmt.Sprintf("\n### %s", content)
 	r.Str += str
 	return r
 }
@@ -240,19 +240,19 @@ func (r *MarkDown) ItalicBold(content string) *MarkDown {
 }
 
 func (r *MarkDown) BlockReference(content string) *MarkDown {
-	str := fmt.Sprintf("\\n> %s\\n", content)
+	str := fmt.Sprintf("\n> %s\n", content)
 	r.Str += str
 	return r
 }
 
 func (r *MarkDown) Image(text, url string, width, height int) *MarkDown {
-	str := fmt.Sprintf("\\n![%s #%vpx #%vpx](%s)", text, width, height, url)
+	str := fmt.Sprintf("\n![%s #%vpx #%vpx](%s)", text, width, height, url)
 	r.Str += str
 	return r
 }
 
 func (r *MarkDown) DividerLine() *MarkDown {
-	str := fmt.Sprintln("\\n ---\\n")
+	str := fmt.Sprintln("\n ---\n")
 	r.Str += str
 	r.Str = strings.TrimSpace(r.Str)
 	return r
@@ -264,13 +264,13 @@ func (r *MarkDown) Text(content string) *MarkDown {
 }
 
 func (r *MarkDown) NewLine() *MarkDown {
-	r.Str += "\\n"
+	r.Str += "\n"
 	return r
 }
 
 func (r *MarkDown) Code(content string) *MarkDown {
-	c := strings.ReplaceAll(strings.ReplaceAll(content, "\t", "\\t"), "\n", "\\n")
-	str := fmt.Sprintf("\\n```\\n%s\\n```\\n", c)
+	c := strings.ReplaceAll(strings.ReplaceAll(content, "\t", "\t"), "\n", "\n")
+	str := fmt.Sprintf("\n```\n%s\n```\n", c)
 	r.Str += str
 	return r
 }
